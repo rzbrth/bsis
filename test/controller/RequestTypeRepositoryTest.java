2f9f6d89b4bffa10c9d85a5172423007208c1256
Devang Tank <devang.tank@gatewaytechnolabs.com>
2013-12-10 14:24:38 +0530

    create junit tests for AddRequestType_Bulk_Transfer_Flag T:3h00

---
+++ test/controller/RequestTypeRepositoryTest.java
@@ -0,0 +1,42 @@
+package test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.requesttype.RequestType;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import repository.RequestTypeRepository;
+import controller.AdminController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:///D:/Project/BSIS/develop/bsis/war/WEB-INF/v2v-servlet.xml" })
+@WebAppConfiguration
+public class RequestTypeRepositoryTest {
+	
+	@Autowired
+	public RequestTypeRepository requestTypesRepository;
+	
+	@Test
+	public void testSaveAllRequestTypes() {
+		try{
+			List<RequestType> allRequestTypes = new ArrayList<RequestType>();
+			RequestType rt = new RequestType();
+			rt.setId(5);
+			rt.setRequestType("Urgent");
+	    rt.setBulkTransfer(false);
+	    allRequestTypes.add(rt);
+	    requestTypesRepository.saveAllRequestTypes(allRequestTypes);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
